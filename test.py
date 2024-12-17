10a45c44a0eb14dd1acf5af7d2b68510696942af
mingyang wen <wen2990061293@gmail.com>
2024-12-17 19:45:53 +0400

    http server check top3

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-deckCards = [[], []]
-with open("outputDeckCards.txt", "a") as file:
-    file.write(", ".join(map(str, deckCards)) + "\n")
+import torch
+
+# Sample tensor
+x = torch.tensor([1, 3, 2])
+
+# Get top-2 values and indices along dim=1 (row-wise)
+values, indices = torch.topk(x, k=2, dim=0)
+print("Values:\n", values)
+print("Indices:\n", indices)
